bf401ac18de4e0cbb5ad5a396411682d4fe496c6
Ermolaev.ID <Ermolaev.ID@yandex.ru>
2025-06-15 14:55:43 +0500

    change ERROR_ICON

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,7 +1,7 @@
 export const ON_ICON = "\u{1F7E2}";
 export const OFF_ICON = "\u{1F534}";
 export const WARNING_ICON = "\u{1F7E8}";
-export const ERROR_ICON = "\u{26D4}";
+export const ERROR_ICON = "\u{1F6AB}";
 export const DEBUG_ICON = "\u{1F41E}";
 
 export const CONFIG_DIR = ".vpn-eid";
